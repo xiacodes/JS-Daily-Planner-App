3890f009980517af600d22cd66e9aee6325fae88
Isabelle Zennah <izennah@griffithsandarmour.com>
2023-02-19 21:34:58 +0000

    Added functions for the todo listing

--- script.js
+++ script.js
@@ -3,4 +3,25 @@ $("document").ready(function () {
 	var timeBlocks = $(".time-block");
 	var currentDay = $("#currentDay");
 	var container = $(".container");
+	var hourNow = parseInt(moment().format("H"));
+	var currentToDoItem = parseInt(moment().format("H"));
+
+	// ---------------------------------------------------- //
+
+	currentDay.text(moment().format("dddd, MMMM do"));
+
+	// FUNCTIONS
+	function creatingToDoList() {
+		timeBlocks.each(function () {});
+	}
+
+	function updateToDoClass(toDoHour) {
+		if (toDoHour === hourNow) {
+			toDo.addClass("present").removeClass("past future");
+		} else if (toDoHour < hourNow) {
+			toDo.addClass("past").removeClass("present future");
+		} else {
+			toDo.addClass("future").removeClass("past present");
+		}
+	}
 });
